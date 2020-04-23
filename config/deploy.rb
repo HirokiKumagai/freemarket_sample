99048f3ddac13b91aaee7798397144c9b257e3c5
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2020-04-24 01:58:34 +0900

    fix2

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.12.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'freemarket_sample_68d'
+set :application, 'freemarket_sample'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:hirokidiv/freemarket_sample.git'
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/hiro_corp.pem'] 
+                  keys: ['~/.ssh/hero_corp.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
