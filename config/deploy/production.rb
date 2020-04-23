7a22029227441dd7bdd3d999de4c9759a55e768b
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2020-04-24 01:55:34 +0900

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.178.116.207', user: 'ec2-user', roles: %w{app db web}
+server '18.182.112.215', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
